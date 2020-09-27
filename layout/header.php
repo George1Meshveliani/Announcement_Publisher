c024e3f8192688a606709487f64612b976dd211a
George1Meshveliani <giorgi.meshveliani@gmail.com>
2020-09-27 23:37:25 +0400

    First run of advanced and secure login system

---
+++ layout/header.php
@@ -0,0 +1,22 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="HeaderDesing.css">
+
+<body>
+
+<div class="header">
+  <div class="header-right">
+  
+    <a href="welcome.php">Home</a>
+    <a class="active" href="profile.php">Profile</a>
+    <a href="register.php">Create New Accountt</a>
+    <a href="login1.php">Log In</a>
+    <a href="logout1.php">Log Out</a>
+    
+
+  </div>
+
+</div>
+
+</body>
---
+++ profile.php
@@ -0,0 +1,9 @@
+
+<html>
+<link rel="stylesheet" href="HeaderDesign.css">
+
+<?php require_once 'layout/header.php' ?>
+
+<div class=ann>Welcome to your profile page</div>
+
+</html>

ec0cf240fde997bd0553876f1210a8ae4b78c297
George1Meshveliani <giorgi.meshveliani@gmail.com>
2020-09-28 00:11:54 +0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -18,5 +18,6 @@ require_once 'addAnnConfig.php';
 <div class=ann>Welcome to your profile page</div>
   <div class=ann>LIL change for check</div>
   <div class=ann>check again</div>
+  <div class=ann>teamwork medium is ready </div>
 
 </html>

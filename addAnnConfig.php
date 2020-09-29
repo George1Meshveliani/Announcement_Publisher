40edec2085b4714c6c6199e839404eeabc9d8c72
George1Meshveliani <giorgi.meshveliani@gmail.com>
2020-09-29 19:24:26 +0400

    Modification

--- addAnnConfig.php
+++ addAnnConfig.php
@@ -1,4 +1,9 @@
 <?php
+
+
+require_once "layout/header.php"; 
+ require_once "server/userData.php";
+ require_once "server/config.php";
 //This page is ready to give user opportunity to make announcements and publish it to homepage
 session_start();
 // check id user is logged in
@@ -6,7 +11,14 @@ if (isset($_COOKIE["PHTARM"])) {
 } elseif (!isset($_SESSION["loggedin"])) {
     header("Location:login.php");
 }
+
+$current_user = array_filter($user_data, function ($var) {
+    return ($var["id"] === $_COOKIE["PHTARM"] || $var["id"] == $_SESSION["id"]);
+  });
+  $current_user = array_values(array_filter($current_user));
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -24,6 +36,7 @@ if (isset($_COOKIE["PHTARM"])) {
     <?php require_once "layout/header.php"  ?>
     <main class="main-container">
         <h1>announcenment page</h1>
+        <h6>Hello, <?php echo $current_user[0]["fullname"]  ?> You can create and publich an announcement here</h6>
     </main>
     <?php require_once "layout/footer.php"  ?>
 
